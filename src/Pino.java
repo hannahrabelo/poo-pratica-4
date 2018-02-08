d4aee795b2fd977d134dac0adeaa2eab67df9c7d
Iedo Rocha <iedo.rocha@gmail.com>
2018-02-08 11:27:00 -0300

    Criação de classes

---
+++ src/Pino.java
@@ -0,0 +1,24 @@
+
+public class Pino {
+	
+	private int topo;
+	private int discos[];
+	private int tamanho;
+	
+	Pino(int tamanho){
+		this.tamanho = tamanho;
+		discos = new int[tamanho];
+	}
+	
+	
+	public void preencher() {
+		for(int i = tamanho; i<=1; i--) {
+			if(i == 1) {
+				discos[i] = tamanho;
+			}
+			else {
+				discos[i]= 
+			}
+		}
+	}
+}
